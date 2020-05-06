63be01c841b2fcdd181b329d73beeb78ecc4bbeb
Jayashri <jayashri1394@gmail.com>
2020-05-06 16:43:29 +0200

    changed dn onformation

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client:'pg',
     connection:{
-        host:'postgresql-regular-20083',
-        user:'jayashri',
-        password:'',
-        database:'smartbrain'
+        host:'process.env.DATABASE_URL',
+        ssl:true
     }
 });
 

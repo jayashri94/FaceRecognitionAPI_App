0af025eb2d4b76aff5ebafbaf3d738351c199401
Jayashri <jayashri1394@gmail.com>
2020-05-06 16:40:56 +0200

    changed the host address

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client:'pg',
     connection:{
-        host:'127.0.0.1',
+        host:'postgresql-regular-20083',
         user:'jayashri',
         password:'',
         database:'smartbrain'
